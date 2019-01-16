8e8e42e063634e63c76d025d35527ef05ba82a75
Elliott Grieco <elliott.grieco@nyu.edu>
2019-01-16 00:38:54 -0500

    Responds to origins from library.nyu.edu

--- handler.js
+++ handler.js
@@ -1,8 +1,16 @@
 'use strict';
 
 const axios = require('axios');
+const { ALLOW_ORIGIN } = process.env;
 
 module.exports['cors-proxy'] = async (event, context) => {
+
+  function wildcardMatch(str, rule) {
+    return new RegExp("^" + rule.split("*").join(".*") + "$").test(str);
+  }
+
+  const allowOrigin = wildcardMatch(event.headers.Origin, ALLOW_ORIGIN) ? event.headers.Origin : null;
+
   let response;
   try {
     const { url } = event.queryStringParameters;
@@ -12,7 +20,7 @@ module.exports['cors-proxy'] = async (event, context) => {
       statusCode: 200,
       body: response.data,
       headers: {
-        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Origin": allowOrigin, // Required for CORS support to work
         "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
         "content-type": response.headers['content-type']
       }
