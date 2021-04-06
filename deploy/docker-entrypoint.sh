8d1fbac8d8aef390ddd2e65d86deb674e0aebb95
BAlter <barnaby.alter@nyu.edu>
2021-04-05 22:09:35 -0400

    Remove trailing slash

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 terraform init -backend-config="bucket=${BACKEND_BUCKET}" \
                -backend-config="key=${BACKEND_KEY}" \
                -backend-config="region=${BACKEND_REGION}" \
-               -backend-config="dynamodb_table=${BACKEND_DYNAMODB_TABLE}" \
+               -backend-config="dynamodb_table=${BACKEND_DYNAMODB_TABLE}"
 
 exec "$@"
